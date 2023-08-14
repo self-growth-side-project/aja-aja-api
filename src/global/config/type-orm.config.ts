05d876ac01e8bad59e2be8558dffdf689a5e374a
DeveloperKGH <developerkgh@gmail.com>
2023-08-14 20:48:30 +0900

    :sparkles: feat : 비밀번호 재설정 이메일 발송

--- src/global/config/type-orm.config.ts
+++ src/global/config/type-orm.config.ts
@@ -8,7 +8,7 @@ export const TypeOrmConfig = (): TypeOrmModuleOptions => ({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   entities: [__dirname + '/../../**/*.{js,ts}'],
-  logging: true,
+  logging: false,
   synchronize: true,
   timezone: '+00:00',
 });
