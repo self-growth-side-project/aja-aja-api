d29b0c32437ad9c7d42f550ff04914c374f8c5b2
DeveloperKGH <developerkgh@gmail.com>
2023-09-11 08:46:42 +0900

    :recycle: 성인의 생각 조회 API 응답 포맷 수정

--- src/question/interface/dto/response/wise-man-opinion.response.ts
+++ src/question/interface/dto/response/wise-man-opinion.response.ts
@@ -3,12 +3,10 @@ import { Question } from '../../../domain/entity/question.entity';
 
 export class WiseManOpinionResponse {
   @Exclude({ toPlainOnly: true }) private readonly _questionId: number;
-  @Exclude({ toPlainOnly: true }) private readonly _questionTitle: string;
   @Exclude({ toPlainOnly: true }) public readonly _wiseManOpinion: string;
 
-  private constructor(questionId: number, questionTitle: string, wiseManOpinion: string) {
+  private constructor(questionId: number, wiseManOpinion: string) {
     this._questionId = questionId;
-    this._questionTitle = questionTitle;
     this._wiseManOpinion = wiseManOpinion;
   }
 
@@ -18,16 +16,11 @@ export class WiseManOpinionResponse {
   }
 
   @Expose()
-  get questionTitle(): string {
-    return this._questionTitle;
-  }
-
-  @Expose()
   get wiseManOpinion(): string {
     return this._wiseManOpinion;
   }
 
   public static fromQuestionEntity(question: Question): WiseManOpinionResponse {
-    return new WiseManOpinionResponse(question.id, question.title, question.wiseManOpinion);
+    return new WiseManOpinionResponse(question.id, question.wiseManOpinion);
   }
 }
