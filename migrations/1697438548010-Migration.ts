248c3767ffb0a72db6947e87395ffc0650e19f9c
DeveloperKGH <developerkgh@gmail.com>
2023-10-16 15:43:27 +0900

    :rocket: Typeorm Migration Test

---
+++ migrations/1697438548010-Migration.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Migration1697438548010 implements MigrationInterface {
+    name = 'Migration1697438548010'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE \`test2\` (\`created_at\` timestamp(3) NOT NULL COMMENT '생성 일시' DEFAULT CURRENT_TIMESTAMP(3), \`updated_at\` timestamp(3) NOT NULL COMMENT '수정 일시' DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3), \`id\` bigint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '고유 식별 ID', \`test\` varchar(320) NOT NULL COMMENT 'test', PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE \`test2\``);
+    }
+
+}
---
+++ src/app/domain/entity/test2.entity.ts
@@ -0,0 +1,13 @@
+import { BaseTimeEntity } from '../../../global/common/domain/entity/base-time.entity';
+import { Column, Entity, Generated, PrimaryColumn } from 'typeorm';
+import { BigintTransformer } from '../../../global/common/infra/transformer/bigint.transformer';
+
+@Entity({ name: 'test2' })
+export class Test2Entity extends BaseTimeEntity {
+  @Generated('increment')
+  @PrimaryColumn({ type: 'bigint', unsigned: true, transformer: new BigintTransformer(), comment: '고유 식별 ID' })
+  public readonly id: number;
+
+  @Column({ type: 'varchar', length: 320, comment: 'test' })
+  public readonly test: string;
+}
